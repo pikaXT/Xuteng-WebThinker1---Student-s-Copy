d01df424548fc25c3ce13f7f7fd3729b6d4f095d
pikaXT <xtxzx.lin@gmail.com>
2024-10-23 08:58:36 +0000

    hi

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,12 @@
-// 
+window.onload = () => {
+    const canvas = document.getElementById("canvasBoard");
+    let ctx = canvas.getContext("2d");
+
+    // ctx.beginPath()
+    // ctx.moveTo(0,0)
+    // ctx.lineTo(400,400)
+    // ctx.stroke()
+    ctx.fillStyle = "rgba(255, 0, 0)"
+    ctx.fillRect(0, 250, 100, 100)
+
+}
