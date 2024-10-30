49f0228e1fc49627de9ef2243596242192be75a0
pikaXT <xtxzx.lin@gmail.com>
2024-10-30 08:49:51 +0000

    done!!!!!!!!!!!!!!!!!!!!!!!!!!

--- script/projects.js
+++ script/projects.js
@@ -1,12 +1,39 @@
+let x, y, dx, dy, canvas, ctx, ballRadius;
 window.onload = () => {
-    const canvas = document.getElementById("canvasBoard");
-    let ctx = canvas.getContext("2d");
+    canvas = document.getElementById("canvasBoard");
+    ctx = canvas.getContext("2d");
 
-    // ctx.beginPath()
-    // ctx.moveTo(0,0)
-    // ctx.lineTo(400,400)
-    // ctx.stroke()
-    ctx.fillStyle = "rgba(255, 0, 0)"
-    ctx.fillRect(0, 250, 100, 100)
+    ballRadius = 5;
 
+    x =canvas.width / 2 - ballRadius / 2;
+    y =canvas.height / 2 - ballRadius / 2;
+    dx = 10;
+    dy = 10;
+
+    draw();
+
+}
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(0, 113, 113)"
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
+    ctx.fill();
+    ctx.closePath();
+
+    if (x + dx > canvas.width - ballRadius ||  x + dx < ballRadius ) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius ||  y + dy < ballRadius ) {
+        dy = -dy;
+    }
+
+    x = x + dx;
+    y = y + dy;
+
+
+
+    requestAnimationFrame(draw);
 }
